3e1806c3a8eaf1dd747815eaa9b6b73123c3f31a
Xephiro9999 <dreadbot8001@gmail.com>
2023-11-08 00:37:23 -0500

    Berserk Functional

--- src/main/java/online/magicksaddon/magicsaddonmod/magic/magicBerserk.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/magic/magicBerserk.java
@@ -1,20 +1,31 @@
 package online.magicksaddon.magicsaddonmod.magic;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import online.kingdomkeys.kingdomkeys.capability.IPlayerCapabilities;
 import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.magic.Magic;
+import online.kingdomkeys.kingdomkeys.network.PacketHandler;
+import online.kingdomkeys.kingdomkeys.network.stc.SCSyncCapabilityPacket;
 import online.magicksaddon.magicsaddonmod.capabilities.IGlobalCapabilitiesMA;
+import online.magicksaddon.magicsaddonmod.capabilities.IPlayerCapabilitiesMA;
 import online.magicksaddon.magicsaddonmod.capabilities.ModCapabilitiesMA;
 import online.magicksaddon.magicsaddonmod.client.sound.MagicSounds;
 
+import online.kingdomkeys.kingdomkeys.capability.PlayerCapabilities;
+
+import javax.swing.*;
+
 
 public class magicBerserk extends Magic {
+
 
     public magicBerserk(ResourceLocation registryName, boolean hasToSelect, int maxLevel) {
         super(registryName, hasToSelect, maxLevel, null);
@@ -23,19 +34,38 @@ public class magicBerserk extends Magic {
 
     @Override
     protected void magicUse(Player player, Player caster, int level, float fullMPBlastMult, LivingEntity lockOnTarget) {
+
         IGlobalCapabilitiesMA globalData = ModCapabilitiesMA.getGlobal(player);
+
         if(globalData != null) {
-            int time = (int) (ModCapabilities.getPlayer(caster).getMaxMP() * ((level * 0.75) + 5) + 5);
+            int time = (int) (ModCapabilities.getPlayer(caster).getMaxMP() * ((level * 0.75) + 5));
             caster.swing(InteractionHand.MAIN_HAND);
-            player.level.playSound(null, player.blockPosition(), MagicSounds.HASTE.get(), SoundSource.PLAYERS, 1F, 1F);
+            player.level().playSound(null, player.blockPosition(), MagicSounds.BERSERK.get(), SoundSource.PLAYERS, 1F, 1F);
             // Effect and Level Modifier
+            IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
+                if (globalData.getBerserkTicks() <= 0) {
+                    // Future color change line below
 
-            if (globalData.getBerserkTicks() <= 0) {
+                    // Levels 0 - 2
+                    switch (level) {
+                        case 0:
+                            playerData.getStrengthStat().addModifier("buff", +3, true);
+                            PacketHandler.sendTo(new SCSyncCapabilityPacket(playerData), (ServerPlayer) player);
+                    break;
+                        case 1:
+                            playerData.getStrengthStat().addModifier("buff", +6 , true);
+                            PacketHandler.sendTo(new SCSyncCapabilityPacket(playerData), (ServerPlayer) player);
+                            break;
+                        case 2:
+                            playerData.getStrengthStat().addModifier("buff", +9 , true);
+                            PacketHandler.sendTo(new SCSyncCapabilityPacket(playerData), (ServerPlayer) player);
+                            break;
 
-                player.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(new AttributeModifier("Berserk", 0.25 + (0.25 * level), AttributeModifier.Operation.ADDITION));
+                    }
+                globalData.setBerserkTicks(time, level);
             }
-            globalData.setBerserkTicks(time, level);
         }
     }
+
 
 }
